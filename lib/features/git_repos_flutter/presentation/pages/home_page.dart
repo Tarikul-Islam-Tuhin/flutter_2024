a07dffdd8b3f92086ff7063387ed2c22fcb1aafc
Tarikul-Islam-Tuhin <tarikul.tuhin@everforth.co.jp>
2024-01-11 23:02:09 +0600

    home page design

---
+++ lib/features/git_repos_flutter/presentation/pages/home_page.dart
@@ -0,0 +1,119 @@
+import 'package:bs23_flutter_task/features/git_repos_flutter/presentation/widgets/card_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/params/params.dart';
+import '../bloc/git_repos_flutter_bloc.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<GitReposFlutterBloc>(context)
+        .add(const GitReposFilteredEventInitialState());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              color: Colors.black,
+              height: size.height * 0.08,
+              width: double.maxFinite,
+              child: BlocBuilder<GitReposFlutterBloc, GitReposFlutterState>(
+                buildWhen: (previous, current) =>
+                    current is GitReposFilterState,
+                builder: (context, state) {
+                  if (state is GitReposFilterState) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            BlocProvider.of<GitReposFlutterBloc>(context).add(
+                              const GitReposFilteredEvent(
+                                params: Params(
+                                    perPage: 4, updated: '', stars: 'stars'),
+                              ),
+                            );
+                          },
+                          child: Text(
+                            'Stars',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: state.params.stars == 'stars'
+                                  ? Colors.blue
+                                  : Colors.white,
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                            onPressed: () {
+                              BlocProvider.of<GitReposFlutterBloc>(context).add(
+                                  const GitReposFilteredEvent(
+                                      params: Params(
+                                          perPage: 4,
+                                          updated: 'updated',
+                                          stars: '')));
+                            },
+                            child: Text(
+                              'Latest',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: state.params.updated == 'updated'
+                                    ? Colors.blue
+                                    : Colors.white,
+                              ),
+                            ))
+                      ],
+                    );
+                  } else {
+                    return const SizedBox.shrink();
+                  }
+                },
+              ),
+            ),
+            BlocConsumer<GitReposFlutterBloc, GitReposFlutterState>(
+              buildWhen: (previous, current) => current is! GitReposActionState,
+              listenWhen: (previous, current) => current is GitReposActionState,
+              listener: (context, state) {
+                if (state is GitReposFilterState) {
+                  BlocProvider.of<GitReposFlutterBloc>(context).add(
+                      GitReposLoadedEvent(
+                          params: Params(
+                              perPage: state.params.perPage,
+                              stars: state.params.stars,
+                              updated: state.params.updated)));
+                }
+              },
+              builder: (context, state) {
+                if (state is GitReposLoading) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state is GitReposLoaded) {
+                  return ListViewScreen(repoList: state.repos);
+                } else if (state is Error) {
+                  return Text(state.message);
+                } else {
+                  return const Text('Nothing Found');
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:bs23_flutter_task/core/params/params.dart';
-import 'package:bs23_flutter_task/features/git_repos_flutter/presentation/bloc/git_repos_flutter_bloc.dart';
 import 'package:bs23_flutter_task/features/git_repos_flutter/presentation/bloc_providers/bloc_providers.dart';
+import 'package:bs23_flutter_task/features/git_repos_flutter/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -35,79 +34,7 @@ class MyApp extends StatelessWidget {
             iconTheme: const IconThemeData(
               color: Colors.black87,
             )),
-        home: const Home(),
-      ),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  void initState() {
-    super.initState();
-    BlocProvider.of<GitReposFlutterBloc>(context)
-        .add(const GitReposFilteredEventInitialState());
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocConsumer<GitReposFlutterBloc, GitReposFlutterState>(
-        buildWhen: (previous, current) => current is! GitReposActionState,
-        listenWhen: (previous, current) => current is GitReposActionState,
-        listener: (context, state) {
-          if (state is GitReposFilterState) {
-            BlocProvider.of<GitReposFlutterBloc>(context).add(
-                GitReposLoadedEvent(
-                    params: Params(
-                        perPage: state.params.perPage,
-                        stars: state.params.stars,
-                        updated: state.params.updated)));
-          }
-        },
-        builder: (context, state) {
-          if (state is GitReposLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (state is GitReposLoaded) {
-            return Column(
-              children: [
-                Text(state.params.perPage.toString()),
-                Text(state.params.stars.toString()),
-                Text(state.params.updated.toString()),
-                ElevatedButton(
-                    onPressed: () {
-                      BlocProvider.of<GitReposFlutterBloc>(context).add(
-                          const GitReposFilteredEvent(
-                              params: Params(
-                                  perPage: 4, updated: 'updated', stars: '')));
-                    },
-                    child: const Text('Updated')),
-                ElevatedButton(
-                    onPressed: () {
-                      BlocProvider.of<GitReposFlutterBloc>(context).add(
-                          const GitReposFilteredEvent(
-                              params: Params(
-                                  perPage: 4, updated: '', stars: 'stars')));
-                    },
-                    child: const Text('Stars')),
-                Text(state.repos.toString()),
-              ],
-            );
-          } else if (state is Error) {
-            return Text(state.message);
-          } else {
-            return const Text('Nothing Found');
-          }
-        },
+        home: const HomePage(),
       ),
     );
   }
